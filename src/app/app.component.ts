3a44169040f6d3fb3a96e44ac7ad1735a8c96f8e
ahmadabdullah247 <ahmadabdullah247@live.com>
2017-05-07 12:55:31 +0500

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,21 @@ export class AppComponent {
   title = 'flash-words';
   dictionary;
   dicList;
+  word;
   constructor(private http: Http) {
     this.http.get('assets/dictionary.json')
       .subscribe(res => {
         this.dictionary = res.json();
         this.dicList = Object.keys(this.dictionary);
-        console.log(this.dicList);
+        this.word=this.dicList.pop();
+        console.log(this.word);
         return;
       });
+  }
+
+  public getOne() {
+    if (this.dicList != undefined) {
+      this.word = this.dicList.pop();
+    }
   }
 }
